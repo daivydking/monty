2e5ce06de9e4a89732b2c4d53c0220f7185e2502
daivydking <radchibuike@gmail.com>
2024-02-01 21:22:15 -0800

    monty language

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* monty code interpreter
+* main - monty code interpreter
 * @argc: number of arguments
 * @argv: monty file location
 * Return: 0 on success
